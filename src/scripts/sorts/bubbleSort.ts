dab18d1fc756494d0f5b7eedacc12c56210d1f0c
bakedPotatoLord <918780@dpsk12.net>
2023-02-27 12:40:27 -0700

    fix bubble sort

--- src/scripts/sorts/bubbleSort.ts
+++ src/scripts/sorts/bubbleSort.ts
@@ -1,21 +1,30 @@
 import { working, arr, active, draw, endSort, setWorking, setActive } from ".."
-import { swap } from "../helpers"
+import { isSorted, swap } from "../helpers"
 
 
 export async function bubbleSort (){  
   let bubbleSortI = 1
   setWorking(true)
 
-  while(bubbleSortI<arr.length){
+  while(true){
     if(arr[bubbleSortI-1] > arr[bubbleSortI]){
       swap(arr,bubbleSortI-1,bubbleSortI)
-      bubbleSortI=1
+
+    }
+
+    if(bubbleSortI >= arr.length - 1){
+      if(isSorted(arr)){
+        break
+      }else{
+        bubbleSortI = 1
+      }
     }else{
       bubbleSortI++
     }
+
     setActive( bubbleSortI)
     draw(arr)
-    await new Promise((res)=>setTimeout(res,0))
+    await new Promise((res)=>setTimeout(res,10))
   }
   endSort()
 }
