d7262170830217052d37e4177019df0eed16f6ea
= <918780@dpsk12.net>
2023-03-31 19:33:23 -0600

    edit typo

--- src/scripts/sorts/bozoSort.ts
+++ src/scripts/sorts/bozoSort.ts
@@ -5,7 +5,7 @@ import { isSorted } from "../helpers";
 
 export async function bozoSort(){
 
-  if(!window.confirm('bozo sort uses exponential time. This means that since each iteration takes ~20ms, and 40! = 815915283247897734345611269596115894272000000000 , this cannot be expected to solve the equation for ~5.175*(10^38) years, or  ~3.8*(10^28) universe ages')){
+  if(!window.confirm('bozo sort uses exponential time. Since each iteration takes ~20ms, and 40! = 815915283247897734345611269596115894272000000000 , this cannot be expected to solve the equation for ~5.175*(10^38) years, or  ~3.8*(10^28) universe ages')){
     return
   }
 
